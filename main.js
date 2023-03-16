9f240e5b6fa04093a7f89dfb21efa12107c31fe8
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-03-16 22:20:34 +0000

    Add event listener to add to cart button

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 import './style.css'
 
+const input = document.getElementById("input-field");
+const addToCart = document.getElementById("add-button");
+
+addToCart.addEventListener('click', () => {
+  console.log(input.value);
+})
+
 
 
