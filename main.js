71917931facadacac2c56895f3920e4c00b7755c
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-03-17 21:54:39 +0000

    Add firebase app setting

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-import './style.scss'
+import './style.scss';
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 
-const input = document.getElementById("input-field");
-const addToCart = document.getElementById("add-button");
+const appSettings = {
+  databaseURL: "https://playground-7f7f7-default-rtdb.europe-west1.firebasedatabase.app/",
+}
+
+const app = initializeApp(appSettings)
+
+console.log(app)
+
+const input = document.getElementById('input-field');
+const addToCart = document.getElementById('add-button');
 
 addToCart.addEventListener('click', () => {
   console.log(input.value);
-})
-
-
-
+});
