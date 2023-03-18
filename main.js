b1b5e053bb7192f9a0120a962cef0410f88ffbc2
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-03-18 17:18:13 +0000

    Add realtime database from firebase to add list of movies

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 import './style.scss';
-import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
+import { getDatabase, ref, push, } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
 
 const appSettings = {
-  databaseURL: "https://playground-7f7f7-default-rtdb.europe-west1.firebasedatabase.app/",
-}
+  databaseURL:
+    'https://playground-7f7f7-default-rtdb.europe-west1.firebasedatabase.app/',
+};
 
-const app = initializeApp(appSettings)
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const moviesInDB = ref(database, 'movies');
 
-console.log(app)
+console.log(database);
 
 const input = document.getElementById('input-field');
 const addToCart = document.getElementById('add-button');
 
 addToCart.addEventListener('click', () => {
-  console.log(input.value);
+  let inputValue = input.value;
+  if (inputValue.length !== 0) {
+    push(moviesInDB, inputValue);
+    console.log(`${inputValue} added to database`);
+  } else {
+    console.log('Please eneter a valid movie title');
+  }
 });
